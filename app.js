4b25b9c0f99b2f0948ec0988cb91d100562a248d
mikebcbc <mikebconstantino@gmail.com>
2017-06-09 18:32:01 -0400

    listenSubmit and getVideos

--- app.js
+++ app.js
@@ -11,16 +11,31 @@ var RESULT_TEMPLATE = (
 
 function listenSubmit() {
 	// Listen to the Submit button
-		// When clicked, clear out input
+	$('.js-submit').click(function(e) {
+		e.preventDefault();
+		var searchTerm = $('.js-query').val();
 		// Pass into AJAX call function
+		getVideos(searchTerm, renderVideo);
+		// When clicked, clear out input
+		$('.js-query').val("");
+	})
 }
 
 function getVideos(query, callback) {
 	// Send AJAX call with query
-	// On success, use callback
+	$.ajax({
+		url: API_URL,
+		data: {
+			part: "snippet",
+			key: config.API_KEY,
+			q: query
+		},
+		success: callback
+	})
 }
 
 function renderVideo(result) {
+	console.log('pew!');
 	var template = $(RESULT_TEMPLATE);
 	// Find each part of the template, and replace text and attributes accordingly.
 	// Return & spit out the template variable
@@ -31,6 +46,8 @@ function appendResults(data) {
 		// Run the renderVideo function for each item, with map method.
 	// Append the results to js-results
 }
+
+$(listenSubmit);
 
 // $($.ajax({
 // 	url: API_URL,
